a7e6e415716f70314bf46f8ba62c57b509a23354
jamil29 <33689725+jamil29@users.noreply.github.com>
2018-04-04 09:41:26 -0500

    Update trabajo 3.cpp

--- trabajo 3.cpp
+++ trabajo 3.cpp
@@ -4,40 +4,21 @@ using std::cin;
 using std::endl;
 
 int main(){
-	int valor1;
-	int valor2;
-	int valor3;
-	int promedio;
-	
+	int valor1,valor2,valor3,promedio;
+
 	cout<<"Dame tu primer numero :";
 	cin>>valor1;
 	cout<<"Dame tu segundo numero :";
 	cin>>valor2;
 	cout<<"Dame tu tercer numero :";
 	cin>>valor3;
-	if(valor1>valor2 and valor1>valor3)
-		cout<<"El valor mayor es "<<valor1;
-	if(valor2>valor1 and valor2>valor3)
-		cout<<"El valor mayor es "<<valor2;
-	if(valor3>valor2 and valor3>valor1)
-		cout<<"El valor mayor es "<<valor3;
-	cout<<" ---- ";
-		
-	if(valor1<valor2 and valor1<valor3)
-		cout<<"El valor menor es "<<valor1;
-	if(valor2<valor1 and valor2<valor3)
-		cout<<"El valor menor es "<<valor2;
-	if(valor3<valor2 and valor3<valor1)
-		cout<<"El valor menor es "<<valor3;
-		
-	cout<<"----";
-	promedio = valor1 + valor2 + valor3;
-	
-		cout<<"El promedio es : "<<promedio/3;
-		
-	
-		
-	
-	
+
+	(valor1>valor2 && valor1>valor3)?cout<<"El valor mayor es "<<valor1:(valor2>valor1 && valor2>valor3)?cout<<"El valor mayor es "<<valor2:cout<<"El valor mayor es "<<valor3;
+
+	(valor1<valor2 && valor1<valor3)?cout<<"El valor menor es "<<valor1:(valor2<valor1 && valor2<valor3)?cout<<"El valor menor es "<<valor2:cout<<"El valor menor es "<<valor3;
+	promedio = valor1 + valor2 + valor3;cout<<"El promedio es : " <<promedio/3;
+
+
 	return 0;
 }
+
