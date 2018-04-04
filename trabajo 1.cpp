3597bb92c5ec9fa660b56b0ef1f3270be9318cff
jamil29 <33689725+jamil29@users.noreply.github.com>
2018-04-04 09:23:33 -0500

    Update trabajo 1.cpp

--- trabajo 1.cpp
+++ trabajo 1.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
+using namespace std;
+
 
 int main(){
 	int valor1=0;
-	cout<<"¿Cuantos años tienes ? ";
-	cin>>valor1;
-	
-	if(valor1>=18)
-		cout<<"Eres mayor de edad";
-	else
-		cout<<"Eres menor de edad";
+	cout<<"Â¿Cuantos aÃ±os tienes ? ";cin>>valor1;(valor1<18)?cout<<"Eres menor de edad":cout<<"Eres mayor de edad";
+
 }
