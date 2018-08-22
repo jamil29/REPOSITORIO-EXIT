ca00d4e77f42527bf2498e4f3dfd64c765409e3d
jamil29 <33689725+jamil29@users.noreply.github.com>
2018-08-22 09:45:49 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     float a;
+     float b;
+
+    cout<<"dame el valor de a"<<endl;
+    cin>>a;
+
+    cout<<"dame el valor de b"<<endl;
+    cin>>b;
+
+    float suma = (a+b);
+    float multiplicion = (a*b);
+    float division = (a/b);
+
+
+    cout<<"la division es "<< division<<endl;
+
+    cout<<"la suma es "<< suma <<endl;
+
+    cout<<"la multiplicacion es "<< multiplicion <<endl;
+
+
+    return 0;
+}
